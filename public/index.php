ef65aeec75b5899dcbb549620367d5d93e89c87b
Silvester eren BA <sylvestereren45@gmail.com>
2026-04-17 22:14:43 +0700

    Test commit

--- public/index.php
+++ public/index.php
@@ -19,20 +19,3 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
 
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Selamat datang</h1>
-</body>
-
-</html>
